39dd9eab03c1c4c9297ffd16a827d7dc2b0333dd
Dmitry Danilov <dmitry.danilov@softformance.com>
2019-03-03 00:09:53 +0200

    Added admin implementation

--- album_agency/landings/admin.py
+++ album_agency/landings/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Landing, LandingImage, TemplateFile, StaticFile
 
-# Register your models here.
+
+class LandingImageInline(admin.TabularInline):
+    model = LandingImage
+    extra = 1
+
+
+@admin.register(Landing)
+class LandingAdmin(admin.ModelAdmin):
+    inlines = [LandingImageInline]
+    prepopulated_fields = {"slug": ("title_page",)}
+    list_display = (
+        'title', 
+        'title_page', 
+        'is_landing_active', 
+        'date_added', 
+        'date_changed'
+    )
+
+
+@admin.register(TemplateFile)
+class TemplateFileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(StaticFile)
+class StaticFileAdmin(admin.ModelAdmin):
+    pass
